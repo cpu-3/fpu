d7117feafb40980d8d9a2a754fc56730ef8914d2
tansei <katsuyoso@yahoo.co.jp>
2019-02-06 13:34:10 +0900

    removed mem module

--- finv2/finv.sv
+++ finv2/finv.sv
@@ -11,14 +11,15 @@ module finv(
 	wire [12:0] a;
 	assign {s,e,i,a} = x;
 
+	reg [35:0] mem [0:1023];
+	initial $readmemb("inv_v4.bin", mem);
+
 	reg s1;
 	reg [7:0] e1;
 	reg [12:0] a1;
 	reg [22:0] c;
 	reg [12:0] g;
 	
-	mem_inv u1(clk, i, {c, g});
-
 	wire [35:0] calc;
 	assign calc = {1'b1,c,12'b0} - g*a1;
 
@@ -27,24 +28,11 @@ module finv(
 		s1 <= s;
 		e1 <= ~(e+1+((|i)||(|a)));
 		a1 <= a;
+		{c,g} <= mem[i];
 		// stage 2
 		y <= {s1,e1,calc[34:12]};
 	end
 
-endmodule
-
-module mem_inv(
-	input wire clk,
-	input wire [9:0] index,
-	output reg [35:0] o_data);
-
-	reg [35:0] mem [0:1023];
-
-	initial $readmemb("inv_v4.bin", mem);
-
-	always @ (posedge clk) begin
-		o_data <= mem[index];
-	end
 endmodule
 
 `default_nettype wire

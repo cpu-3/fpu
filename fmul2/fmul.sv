7889fc3eaabc4008b3e22fae63bb94f4c7cbb0f7
tansei <katsuyoso@yahoo.co.jp>
2019-02-16 17:37:33 +0900

    fmul opt

--- fmul2/fmul.sv
+++ fmul2/fmul.sv
@@ -6,7 +6,6 @@ module fmul(
 		input wire [31:0] x2,
 		output reg [31:0] y);
 
-//stage 0
 	wire s1;
 	wire s2;
 	wire [7:0] e1;
@@ -17,31 +16,31 @@ module fmul(
 	assign {s2,e2,m2} = x2;
 
 //stage 1
+	wire [47:0] calc;
+	assign calc = {1'b1,m1} * {1'b1,m2};
+
 	reg sy;
 	reg [8:0] esum;
-	reg [47:0] calc;
-	reg z;
+	reg [25:0] calcr;
+	reg nz;
 
 //stage 2
-	wire [46:0] keta;
+	wire [23:0] mketa;
 	wire [22:0] my;
-	assign keta = (calc[47])? calc[46:0]: {calc[45:0],1'b0};
-	assign my = ((~(|keta[22:0])) & keta[23])? keta[46:24] + keta [24]: keta[46:24] + keta[23];
-// round to even
+	assign mketa = (calcr[25])? calcr[24:1]: calcr[23:0];
+	assign my = mketa[23:1] + mketa[0];
 
-	wire [8:0] eketa;
 	wire [8:0] ey;
-	assign eketa = esum + calc[47] + (&keta[46:23]);
-	assign ey = (eketa[8] & eketa[7])? 9'b111111111 :((eketa[8] | eketa[7])? eketa - 9'b001111111: 0);
+	assign ey = esum + (calcr[25] | (&calcr[23:0]));
 
 	always@(posedge clk) begin
 	//stage 1
 		sy <= s1 ^ s2;
-		esum <= e1 + e2;
-		calc = {1'b1,m1} * {1'b1,m2};
-		z <= (|e1) && (|e2);
+		esum <= e1 + e2 - 9'b001111111;
+		calcr <= calc[47:22];
+		nz <= (|e1) & (|e2);
 	//stage 2
-		y <= (z)? {sy,ey[7:0],my}: {sy,31'b0};
+		y <= (nz)? {sy,ey[7:0],my}: {sy,31'b0};
 	end
 
 endmodule

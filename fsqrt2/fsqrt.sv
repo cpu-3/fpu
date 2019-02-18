0a18c1008a6897a4cf14be5f7ff3e79b54a271cb
tansei <katsuyoso@yahoo.co.jp>
2019-02-18 15:55:35 +0900

    minor change

--- fsqrt2/fsqrt.sv
+++ fsqrt2/fsqrt.sv
@@ -19,20 +19,22 @@ module fsqrt(
 	reg [13:0] a1;
 	reg [22:0] c;
 	reg [13:0] g;
+	reg nz;
 	
-	wire [6:0] em;
+	wire [6:0] ea;
 	wire [36:0] calc;
-	assign em = e - {6'b0,~i[9]};
+	assign ea = e - {6'b0,~i[9]};
 	assign calc = {c,14'b0} + g*a1;
 
 	always@(posedge clk) begin
 		// stage 1
 		{c,g} <= mem[i];
 		s1 <= s;
-		e1 <= {em[6],~em[6],em[5:0]};
+		e1 <= {ea[6],~ea[6],ea[5:0]};
 		a1 <= a;
+		nz <= (|e || i[9]);
 		// stage 2
-		y <= (e1 == 8'b10111111)? {s1,31'b0}: {s1,e1,calc[36:14]+calc[13]};
+		y <= (nz)? {s1,e1,calc[36:14]+calc[13]}: {s1,31'b0};
 	end
 endmodule
 
